0050a898eceb9e1ca7c7968ce87fbc7baa425be8
tanujkumbhar <87263352+tanujkumbhar@users.noreply.github.com>
2023-01-29 00:15:10 +0530

    Login Changes

--- NiceAdmin/PHP/index.php
+++ NiceAdmin/PHP/index.php
@@ -3,25 +3,54 @@
   require_once('includes/load.php');
   if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
 ?>
-<?php include_once('layouts/header.php'); ?>
-<div class="login-page">
-    <div class="text-center">
-       <h1>Login Panel</h1>
-       <h4>Inventory Management System</h4>
-     </div>
-     <?php echo display_msg($msg); ?>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial scale=1.0" >
+	<title>Login Form</title>
+	<link rel="stylesheet" type="text/css" href="../LAND/assets/css/styles.css">
+</head>
+
+
+	<section>
+		<div class="color"></div>
+		<div class="color"></div>
+		<div class="color"></div>
+		<div class="box">
+		<div class="square" style="--i:0;"></div>
+		<div class="square" style="--i:1;"></div>
+		<div class="square" style="--i:2;"></div>
+		<div class="square" style="--i:3;"></div>
+		<div class="square" style="--i:4;"></div>
+    <div class="login-page">
+        <div class="container">
+            <div class="center">
+			<div class="form">
+				<h2>Login Form</h2>
+        <?php echo display_msg($msg); ?>
       <form method="post" action="auth.php" class="clearfix">
         <div class="form-group">
-              <label for="username" class="control-label">Username</label>
-              <input type="name" class="form-control" name="username" placeholder="Username">
+					<div class="inputBox">
+          <input type="name" class="form-control" name="username" placeholder="Username">
+					</div>
+          </div>
+          <div class="form-group">
+					<div class="inputBox">
+          <input type="password" name= "password" class="form-control" placeholder="Password">
+					</div>
+          </div>
+          <div class="form-group">
+          <div class="inputBox button" >
+          <input type="Submit" class="btn btn-danger" value="Login" style="border-radius:0%"></input>
+          </div>
+				</form>
+			</div>
         </div>
-        <div class="form-group">
-            <label for="Password" class="control-label">Password</label>
-            <input type="password" name= "password" class="form-control" placeholder="Password">
-        </div>
-        <div class="form-group">
-                <button type="submit" class="btn btn-danger" style="border-radius:0%">Login</button>
-        </div>
-    </form>
+		</div>
+	</div>
 </div>
+	</section>
+
+
+
 <?php include_once('layouts/footer.php'); ?>
